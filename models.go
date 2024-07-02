a669ec839ca776d6d29a235d77eb1dabde936d18
LazyLinchen <1193782678@qq.com>
2024-07-02 09:31:26 +0800

    Update models.go

--- models.go
+++ models.go
@@ -4,13 +4,14 @@ const (
 	ErnieBot      = "erniebot"
 	ErnieBotTurBo = "erniebot-turbo"
 	BLOOMZ7B      = "BLOOMZ-7B"
-	
+	ERNIE_4       = "ERNIE-4"
 )
 
 var enableChatCompletionModels = map[string]string{
 	ErnieBot:      "/wenxinworkshop/chat/completions",
 	ErnieBotTurBo: "/wenxinworkshop/chat/eb-instant",
 	BLOOMZ7B:      "/wenxinworkshop/chat/bloomz_7b1",
+	ERNIE_4:       "/wenxinworkshop/chat/completions_pro",
 }
 
 func isSupportedChatCompletionModel(model string) bool {
